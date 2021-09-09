7844340010164d4b665ada9a79594fb78cc1a31a
Robin Lundin Söderberg <hed20rlg@cs.umu.se>
2021-09-09 19:00:49 +0200

    No valgrind warnings

--- mexec.c
+++ mexec.c
@@ -6,24 +6,43 @@
 #include <dirent.h>
 #include <string.h>
 
+/**
+ * @author Robin Lundin Söderberg
+ * cs:hed20rlg
+ * Date: 2021-00-00
+ * Description: 
+ * 
+ * 
+ */
 
 int readCommands(char* buff, char* string, int argc, char* argv[]);
 int numCommands(char* string, int commandsFrom);
 
 int main(int argc, char *argv[]) {
 
-    char* buff = malloc(sizeof(buff) + 1);
-    char* string = malloc(sizeof(string) + 1);
-    pid_t PID;
-    int fd[2];
+    char* buff = malloc(1024 * sizeof(*buff) + 1);
+    char* string = malloc(1024 * sizeof(*string) + 1);
+    if(buff == NULL){
+        fprintf(stderr, "Malloc failed");
+        exit(EXIT_FAILURE);
+    } 
+
+    string[0] = '\0';
 
     //Get the commands to string[].
     int numOfCommands = readCommands(buff, string, argc, argv);
+    
 
-    if(pipe(fd) != 0){
-        perror("pipe didnt work");
-        exit(EXIT_FAILURE);
-    }
+    
+
+    
+
+    printf("%s", string);
+    printf("%d", numOfCommands);
+    
+    
+
+    
 
     free(buff);
     free(string);
@@ -37,7 +56,7 @@ int main(int argc, char *argv[]) {
  * @param string Where to save all the in data on.
  * @param argc How many arguments was it at start.
  * @param argv Array of all the arguments.
- * @return Returns number of commands.
+ * @return Returns 0 = commands from file, 1 = commands from user.
  */
 int readCommands(char* buff, char* string, int argc, char* argv[]){
 
@@ -63,15 +82,16 @@ int readCommands(char* buff, char* string, int argc, char* argv[]){
             exit(EXIT_FAILURE);
 
         }else {
-
+            
             while(fgets(buff, sizeof(buff), fp) != NULL) {
 
                 //Check size of the commands, max length is 1024 characters.
                 if (strlen(buff) <= 1024) {
                     strncat(string, buff, strlen(buff));
-                }      
+                }
+                
             }
-
+            fclose(fp);
             return numCommands(string, commandsFrom);
         }
 
@@ -87,29 +107,18 @@ int readCommands(char* buff, char* string, int argc, char* argv[]){
         
         //Set the pointer to the beginning of the stdin buffer.
         fseek(stdin, 0, SEEK_SET);
+        
+        while (fgets(buff, sizeof(buff), stdin) != NULL){
 
-        //Reads from stdin buffer in to an array.
-        fread(buff, sizeof(buff) + 1, 1, stdin);
-        int j = 0;
-        int k = 0;
-
-        //Checking so the commands is not over 1024 characters.
-        while (buff[j] != '\0'){
-            if(buff[j] != '\n') {
-                k++;
-            } else if(k > 1024) {
-                fprintf(stderr, "One command is longer then 1024 char");
-                exit(EXIT_FAILURE);
+            
+            if(strlen(buff) <= 1024) {
+                strcat(string, buff);
             }
-            else {
-                k = 0;
-            }
-            j++;
+            
+            
+            
         }
-
-        //Copy the commands over to an array of all the commands.
-        strncat(string, buff, strlen(buff));
-
+        
         return numCommands(string, commandsFrom);
            
     } else {
@@ -117,9 +126,12 @@ int readCommands(char* buff, char* string, int argc, char* argv[]){
         //Reads stdin from user until ctrl-d, and check if the commands is under 1025 charcters.
         while (fgets(buff, sizeof(buff), stdin) != NULL){
 
+            
+
             if(strlen(buff) <= 1024) {
-                strncat(string, buff, strlen(buff));
+                strcat(string, buff);
             }
+            
             
         }
 
@@ -139,7 +151,7 @@ int readCommands(char* buff, char* string, int argc, char* argv[]){
 int numCommands(char* string, int commandsFrom) {
 
     int commands = 0;
-    int i = 0;
+    size_t i = 0;
     while(i <= strlen(string)) {
 
         if(string[i] == '\n') {
@@ -147,7 +159,7 @@ int numCommands(char* string, int commandsFrom) {
         }
         i++;
     }
-
+    
     //last command ends with '\0' from files and do not have an '\n'.
     if(commandsFrom == 0) {
         commands++;  
